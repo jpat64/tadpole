d633b4f70ebd920d1d6cccd9a569165ea095d756
jpat64 <jimmy@pattersonadvice.com>
2023-10-27 16:56:13 -0500

    compiler warnings

--- lib/models/EntryModel.dart
+++ lib/models/EntryModel.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: file_names
 
-import 'dart:ui';
-
 import 'package:decimal/decimal.dart';
 import 'dart:convert';
 import 'dart:io';
--- lib/services/Pair.dart
+++ lib/services/Pair.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 class Pair<E, F> {
   E first;
   F last;
