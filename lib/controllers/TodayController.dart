e4eaad13648a94b87a47730918273b1bd525f242
jpat64 <jimmy@pattersonadvice.com>
2023-11-21 09:57:19 -0600

    initial commit (#6)

--- lib/controllers/TodayController.dart
+++ lib/controllers/TodayController.dart
@@ -6,13 +6,17 @@ import 'package:tadpole/models/EntryModel.dart';
 import 'package:tadpole/services/StorageService.dart';
 
 class TodayController extends BaseController {
-  Future<bool> addEntry(bool bleeding, List<Symptom>? symptoms) async {
+  Future<bool> addEntry(bool bleeding, int? painLevel, int? flowLevel,
+      List<Symptom>? symptoms, List<Activity>? activities) async {
     Entry entry = Entry(
       cycle:
           await storageService.getStoredInt(StorageService.NEXT_CYCLE_NUMBER),
       id: getTodayId(),
+      pain: painLevel,
+      flow: flowLevel,
       bleeding: bleeding,
       symptoms: symptoms,
+      activities: activities,
     );
     print("incoming entry: $entry");
     storageService.updateEntry(entry);
--- lib/models/ThemeModel.dart
+++ lib/models/ThemeModel.dart
@@ -2,6 +2,9 @@
 
 class ThemeModel {
   String bleedingQuestion;
+  String painQuestion;
+  String flowQuestion;
+  String temperatureQuestion;
   String symptomQuestion;
   String activityQuestion;
   int id;
@@ -9,6 +12,9 @@ class ThemeModel {
   ThemeModel({
     required this.id,
     required this.bleedingQuestion,
+    required this.painQuestion,
+    required this.flowQuestion,
+    required this.temperatureQuestion,
     required this.symptomQuestion,
     required this.activityQuestion,
   });
@@ -17,6 +23,12 @@ class ThemeModel {
     return ThemeModel(
       id: json['id'] ?? 0,
       bleedingQuestion: json['bleeding_question'] ?? "Are you bleeding?",
+      painQuestion:
+          json['pain_question'] ?? "From 1 to 5, what's your pain level?",
+      flowQuestion:
+          json['flow_question'] ?? "From 1 to 5, how heavy is your flow?",
+      temperatureQuestion:
+          json['temperature_question'] ?? "What's your temperature?",
       symptomQuestion: json['symptom_question'] ?? "What are your symptoms?",
       activityQuestion:
           json['activity_question'] ?? "Which activities have you done?",
@@ -26,12 +38,18 @@ class ThemeModel {
   static ThemeModel base({
     id = 0,
     bleedingQuestion = "Are you bleeding?",
+    painQuestion = "From 1 to 5, what's your pain level?",
+    flowQuestion = "From 1 to 5, how heavy is your flow?",
+    temperatureQuestion = "What's your temperature?",
     symptomQuestion = "What are your symptoms?",
     activityQuestion = "Which activities have you done?",
   }) {
     return ThemeModel(
       id: id,
       bleedingQuestion: bleedingQuestion,
+      painQuestion: painQuestion,
+      flowQuestion: flowQuestion,
+      temperatureQuestion: temperatureQuestion,
       symptomQuestion: symptomQuestion,
       activityQuestion: activityQuestion,
     );
@@ -41,6 +59,9 @@ class ThemeModel {
     return {
       "id": id,
       "bleeding_question": bleedingQuestion,
+      "pain_question": painQuestion,
+      "flow_question": flowQuestion,
+      "temperature_question": temperatureQuestion,
       "symptom_question": symptomQuestion,
       "activity_question": activityQuestion,
     };
